f4f2ca5501a3ccaf3805e5e71b6277a8f6e6a90e
Dohoon Gwak <dogwak@cx2r6s8.42seoul.kr>
2023-11-27 21:56:08 +0900

    implement test codes for vector and string's vector.

---
+++ test/vec_str_test.c
@@ -0,0 +1,81 @@
+// cc vec_str_test.c -L../FT_Vector -lftvector -L../FT_String -lftstring -o vec_str_test
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "../FT_Vector/ft_vector.h"
+#include "../FT_String/ft_string.h"
+void check_leak(void);
+
+int construct_ft_string_in_vec(void *paddr, void *literal)
+{
+	*(t_ft_string **)paddr = construct_ftstr_cstr((char *)literal);
+	if (paddr == NULL)
+		return (0);
+	return (1);
+}
+
+void destruct_ft_string_in_vec(void *paddr)
+{
+	destruct_ftstr(*(t_ft_string **)paddr);
+}
+
+void print_all(t_ft_vector *str_vec)
+{
+	for (int i = 0; i < str_vec->size; ++i)
+	{
+		//printf("%d : ", i);
+		write(1, (*(t_ft_string **)(str_vec->at(str_vec, i)))->pbuffer,
+		(*(t_ft_string **)(str_vec->at(str_vec, i)))->size);
+		write(1, "\n", 1);
+	}
+}
+
+int main()
+{
+	char *str1 = "start";
+	char *str2 = "hello world";
+	char *str3 = "end";
+
+	// constructor test
+	printf("call constructor\n");
+	t_ft_vector *str_vec = construct_ftvec(construct_ft_string_in_vec,
+		destruct_ft_string_in_vec, sizeof(t_ft_string *));
+	printf("check is empty : %d \n", str_vec->empty(str_vec));
+
+	// push_back test
+	printf("call push_back\n");
+	str_vec->push_back(str_vec, str1);
+	for (int i = 0; i < 12; ++i)
+		str_vec->push_back(str_vec, str2);
+	str_vec->push_back(str_vec, str3);
+
+	// content check
+	printf("call at, check content.\n");
+	print_all(str_vec);
+
+	// resize
+	printf("call resize, change to 5, check content.\n");
+	str_vec->resize(str_vec, 5);
+	print_all(str_vec);
+	printf("check is empty : %d \n", str_vec->empty(str_vec));
+
+	// clear
+	printf("call clear, check content.\n");
+	str_vec->clear(str_vec);
+	print_all(str_vec);
+	printf("check is empty : %d \n", str_vec->empty(str_vec));
+
+	// destruct vector
+	printf("calling destructor.\n");
+	destruct_ftvec(str_vec);
+
+	printf("========================\n");
+	atexit(check_leak);
+	return (0);
+}
+
+void check_leak(void)
+{
+	system("leaks --list -- vec_str_test");
+}
--- test/vec_test.c
+++ test/vec_test.c
@@ -1,8 +1,101 @@
 // cc vec_test.c -L../FT_Vector -lftvector -o vec_test
 #include <stdio.h>
+#include <stdlib.h>
 #include "../FT_Vector/ft_vector.h"
+
+typedef struct test_struct
+{
+	int number;
+	char *str;
+} t_test;
+
+struct param_test
+{
+	int n;
+	char *str;
+};
+
+int construct_test(void *paddr, void *pparam)
+{
+	((t_test *)paddr)->number = ((struct param_test *)pparam)->n;
+	((t_test *)paddr)->str = malloc(((struct param_test *)pparam)->n * sizeof(char));
+	if (((t_test *)paddr)->str == NULL)
+	{
+		return (0);
+	}
+	for (int i = 0; i < ((struct param_test *)pparam)->n; ++i)
+	{
+		((t_test *)paddr)->str[i] = ((struct param_test *)pparam)->str[i];
+	}
+	return (1);
+}
+
+void destruct_test(void *paddr)
+{
+	if (paddr != NULL)
+	{
+		free(((t_test *)paddr)->str);
+	}
+}
+
+void check_leak(void)
+{
+	system("leaks --list -- vec_test");
+}
+
+void print_test_vec_all(t_ft_vector *vec)
+{
+	for (int i = 0; i < vec->size; ++i)
+	{
+		printf("%s\n", ((t_test *)(vec->at(vec, i)))->str);
+	}
+}
 
 int main()
 {
+	// basic check.
+	struct param_test param1 = {12, "hello world"};
+	struct param_test param2 = {6, "start"};
+	struct param_test param3 = {4, "end"};
 
+	// constructor test
+	printf("call constructor\n");
+	t_ft_vector *test_vec = construct_ftvec(construct_test,
+		destruct_test, sizeof(t_test));
+	printf("check is empty : %d \n", test_vec->empty(test_vec));
+
+	// push_back test
+	printf("call push_back\n");
+	test_vec->push_back(test_vec, &param2);
+	for (int i = 0; i < 12; ++i)
+		test_vec->push_back(test_vec, &param1);
+	test_vec->push_back(test_vec, &param3);
+
+	// content check
+	printf("call at, check content.\n");
+	print_test_vec_all(test_vec);
+
+	// front, back check
+	printf("call ft_vector front : %s\n", ((t_test *)(test_vec->front(test_vec)))->str);
+	printf("call ft_vector back  : %s\n", ((t_test *)(test_vec->back(test_vec)))->str);
+
+	// resize
+	printf("call resize, change to 5, check content.\n");
+	test_vec->resize(test_vec, 5);
+	print_test_vec_all(test_vec);
+	printf("check is empty : %d \n", test_vec->empty(test_vec));
+
+	// clear
+	printf("call clear, check content.\n");
+	test_vec->clear(test_vec);
+	print_test_vec_all(test_vec);	// do nothing
+	printf("check is empty : %d \n", test_vec->empty(test_vec));
+
+	// destruct vector
+	printf("calling destructor.\n");
+	destruct_ftvec(test_vec);
+
+	printf("========================\n");
+	atexit(check_leak);
+	return (0);
 }
