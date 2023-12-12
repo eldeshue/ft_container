3edf540437041fc0d40f1a7b5df59145ddb25d00
Dohoon Gwak <dogwak@cx2r6s8.42seoul.kr>
2023-12-12 14:39:06 +0900

    modify test code, delete unnecessary elf file

--- test/deque_test.c
+++ test/deque_test.c
@@ -10,7 +10,7 @@ void check_leak(void);
 
 int construct_ft_string_in_deque(void *paddr, void *literal)
 {
-	*(t_ft_string **)paddr = construct_ftstr_cstr((char *)literal);
+	*(t_ft_string *)paddr = construct_ftstr_cstr((char *)literal);
 	if (paddr == NULL)
 		return (0);
 	return (1);
@@ -18,12 +18,12 @@ int construct_ft_string_in_deque(void *paddr, void *literal)
 
 void destruct_ft_string_in_deque(void *paddr)
 {
-	destruct_ftstr(*(t_ft_string **)paddr);
+	destruct_ftstr((t_ft_string *)paddr);
 }
 
-void print_ft_string_nl(t_ft_string **str)
+void print_ft_string_nl(t_ft_string *str)
 {
-	write(1, (*str)->pbuffer, (*str)->size);
+	write(1, (str)->pbuffer, (str)->size);
 	write(1, "\n", 1);
 }
 int main()
@@ -34,69 +34,69 @@ int main()
 
 	// constructor test
 	printf("call constructor\n");
-	t_ft_deque *str_deque = construct_ftdeque(construct_ft_string_in_deque,
-											  destruct_ft_string_in_deque, sizeof(t_ft_string *));
-	printf("check is empty : %d \n", str_deque->empty(str_deque));
+	t_ft_deque str_deque = construct_ftdeque(construct_ft_string_in_deque,
+											  destruct_ft_string_in_deque, sizeof(t_ft_string));
+	printf("check is empty : %d \n", str_deque.empty(&str_deque));
 
 	// push test
 	printf("call push_back\n");
-	str_deque->push_back(str_deque, str1);
-	printf("check is empty : %d \n", str_deque->empty(str_deque));
+	str_deque.push_back(&str_deque, str1);
+	printf("check is empty : %d \n", str_deque.empty(&str_deque));
 	printf("front is : ");
-	print_ft_string_nl(str_deque->front(str_deque));
+	print_ft_string_nl(str_deque.front(&str_deque));
 	printf("back is : ");
-	print_ft_string_nl(str_deque->back(str_deque));
+	print_ft_string_nl(str_deque.back(&str_deque));
 
 	printf("call push_back\n");
-	str_deque->push_back(str_deque, str2);
+	str_deque.push_back(&str_deque, str2);
 	printf("front is : ");
-	print_ft_string_nl(str_deque->front(str_deque));
+	print_ft_string_nl(str_deque.front(&str_deque));
 	printf("back is : ");
-	print_ft_string_nl(str_deque->back(str_deque));
+	print_ft_string_nl(str_deque.back(&str_deque));
 
 	printf("call push_back\n");
-	str_deque->push_back(str_deque, str3);
+	str_deque.push_back(&str_deque, str3);
 	printf("front is : ");
-	print_ft_string_nl(str_deque->front(str_deque));
+	print_ft_string_nl(str_deque.front(&str_deque));
 	printf("back is : ");
-	print_ft_string_nl(str_deque->back(str_deque));
+	print_ft_string_nl(str_deque.back(&str_deque));
 
 	printf("call push_front\n");
-	str_deque->push_front(str_deque, str2);
+	str_deque.push_front(&str_deque, str2);
 	printf("front is : ");
-	print_ft_string_nl(str_deque->front(str_deque));
+	print_ft_string_nl(str_deque.front(&str_deque));
 	printf("back is : ");
-	print_ft_string_nl(str_deque->back(str_deque));
+	print_ft_string_nl(str_deque.back(&str_deque));
 
 	printf("call push_front\n");
-	str_deque->push_front(str_deque, str1);
+	str_deque.push_front(&str_deque, str1);
 	printf("front is : ");
-	print_ft_string_nl(str_deque->front(str_deque));
+	print_ft_string_nl(str_deque.front(&str_deque));
 	printf("back is : ");
-	print_ft_string_nl(str_deque->back(str_deque));
+	print_ft_string_nl(str_deque.back(&str_deque));
 
 	// clear check
 	printf("call clear, check content.\n");
-	str_deque->clear(str_deque);
-	printf("check is empty : %d \n", str_deque->empty(str_deque));
+	str_deque.clear(&str_deque);
+	printf("check is empty : %d \n", str_deque.empty(&str_deque));
 
 	printf("call push_back\n");
-	str_deque->push_back(str_deque, str3);
+	str_deque.push_back(&str_deque, str3);
 	printf("front is : ");
-	print_ft_string_nl(str_deque->front(str_deque));
+	print_ft_string_nl(str_deque.front(&str_deque));
 	printf("back is : ");
-	print_ft_string_nl(str_deque->back(str_deque));
+	print_ft_string_nl(str_deque.back(&str_deque));
 
 	printf("call push_front\n");
-	str_deque->push_front(str_deque, str2);
+	str_deque.push_front(&str_deque, str2);
 	printf("front is : ");
-	print_ft_string_nl(str_deque->front(str_deque));
+	print_ft_string_nl(str_deque.front(&str_deque));
 	printf("back is : ");
-	print_ft_string_nl(str_deque->back(str_deque));
+	print_ft_string_nl(str_deque.back(&str_deque));
 
 	// destruct vector
 	printf("calling destructor.\n");
-	destruct_ftdeque(str_deque);
+	destruct_ftdeque(&str_deque);
 
 	printf("========================\n");
 	atexit(check_leak);
