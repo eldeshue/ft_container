0813b4b9f998f2ac82ad4456c1a04544977733b6
iphuck22 <eldeshue@naver.com>
2023-12-24 20:00:50 +0900

    modify getword method

--- ft_string/ft_string_utility.c
+++ ft_string/ft_string_utility.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_string_utility.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dogwak <dogwak@student.42.fr>              +#+  +:+       +#+        */
+/*   By: dogwak <dogwak@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/20 13:55:02 by dogwak            #+#    #+#             */
-/*   Updated: 2023/12/22 15:19:01 by dogwak           ###   ########.fr       */
+/*   Updated: 2023/12/24 17:15:54 by dogwak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,8 @@
 
 int	ft_str_compare(t_ft_string *this, t_ft_string *ftstr)
 {
-	size_t		cmp_size;
-	size_t		idx;
+	size_t	cmp_size;
+	size_t	idx;
 
 	cmp_size = this->size;
 	if (this->size > ftstr->size)
@@ -41,50 +41,59 @@ char	*ft_str_c_str(t_ft_string *this)
 
 int	ft_str_getline(t_ft_string *this, int fd)
 {
-	char		input_char;
-	ssize_t		read_size;
-	ssize_t		total_size;
+	char	input_char;
+	ssize_t	read_size;
+	ssize_t	total_size;
 
 	if (fd < 0)
 		return (-1);
 	this->size = 0;
 	input_char = -1;
 	total_size = 0;
-	read_size = read(fd, &input_char, 1);
-	while (read_size > 0 && input_char != '\n')
+	while (1)
 	{
-		if (!ft_str_push_back(this, input_char))
-			return (-1);
 		read_size = read(fd, &input_char, 1);
-		if (read_size < 0)
+		if (read_size == 0 || input_char == '\n')
+			break ;
+		if (read_size < 0 || !ft_str_push_back(this, input_char))
 			return (-1);
-		total_size += read_size;
+		total_size++;
 	}
 	return (total_size);
 }
 
+static int	is_space(char c)
+{
+	if (c != ' ' || c != '\t' || c != '\n' || c != '\v' || c != '\f'
+		|| c != '\r')
+		return (1);
+	return (0);
+}
+
 int	ft_str_getword(t_ft_string *this, int fd)
 {
-	char		input_char;
-	ssize_t		read_size;
-	ssize_t		total_size;
+	char	input_char;
+	ssize_t	read_size;
+	ssize_t	total_size;
+	int		read_flag;
 
 	if (fd < 0)
 		return (-1);
 	this->size = 0;
 	input_char = -1;
 	total_size = 0;
-	read_size = read(fd, &input_char, 1);
-	while (read_size > 0
-		&& (input_char != ' ' || input_char != '\t' || input_char != '\n'
-			|| input_char != '\v' || input_char != '\f' || input_char != '\r'))
+	read_flag = 0;
+	while (1)
 	{
-		if (!ft_str_push_back(this, input_char))
-			return (-1);
 		read_size = read(fd, &input_char, 1);
-		if (read_size < 0)
+		if (!read_flag && read_size > 0 && is_space(input_char))
+			continue ;
+		read_flag = 1;
+		if (read_size == 0 || is_space(input_char))
+			break ;
+		if (read_size < 0 || !ft_str_push_back(this, input_char))
 			return (-1);
-		total_size += read_size;
+		total_size++;
 	}
 	return (total_size);
 }
